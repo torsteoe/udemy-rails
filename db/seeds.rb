2bb605f03bf8313a8624cbc0e7a617ce6f7b1b78
torsteoe <torsteoe@stud.ntnu.no>
2019-09-17 22:40:22 +0200

    implement custom scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+	)
+end
 10.times do |blog|
 	Blog.create!(
 		title: "My blog Post #{blog}",
-		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		topic_id: Topic.last.id
 )
 end
 
@@ -16,14 +22,24 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
 	Portfolio.create!(
 		title: "Portfolio title: #{portfolio_item}",
-		subtitle: "my great service",
+		subtitle: "Ruby",
 		body: "latin text",
 		main_image: "http://placehold.it/600x400",
 		thumb_image: "http://placehold.it/350x150"
 )
 end
+1.times do |portfolio_item|
+        Portfolio.create!(
+                title: "Portfolio title: #{portfolio_item}",
+                subtitle: "Angular",
+                body: "latin text",
+                main_image: "http://placehold.it/600x400",
+                thumb_image: "http://placehold.it/350x150"
+)
+end
+
 
 puts "9 portfolio items created"
