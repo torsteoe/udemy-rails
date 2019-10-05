2f75a25506fba5631f8b1ff7119e38b731c898b7
torsteoe <torsteoe@stud.ntnu.no>
2019-10-05 10:29:06 +0200

    add pry-byebug and make set portfolio item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+    before_action :et_portfolio_item, only: [:edit, :update, :show, :destroy]
     layout 'portfolio'
 	def index
 		@portfolio_items = Portfolio.all
@@ -26,11 +27,9 @@ class PortfoliosController < ApplicationController
 	end
 	
 	def edit
-		@portfolio_item = Portfolio.find(params[:id])
 	end	
 
 	def update
-		@portfolio_item = Portfolio.find(params[:id])
 		respond_to do |format|
 			if @portfolio_item.update(portfolio_params)
                 format.html { redirect_to portfolios_path, notice: 'your post is live' }
@@ -41,11 +40,9 @@ class PortfoliosController < ApplicationController
 	end
 	
 	def show
-		@portfolio_item = Portfolio.find(params[:id])
 	end
 
 	def destroy
-		@portfolio_item = Portfolio.find(params[:id])
 		@portfolio_item.destroy
 		respond_to do |format|
 			format.html { redirect_to portfolios_url, notice: 'Item removed'}
@@ -60,5 +57,8 @@ class PortfoliosController < ApplicationController
                                          technologies_attributes: [:name]
                                          )
 
+    end
+    def set_portfolio_item
+        @portfolio_item = Portfolio.find(params[:id])
     end
 end
