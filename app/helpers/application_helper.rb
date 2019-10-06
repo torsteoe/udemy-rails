0bff85451b3d3d81e99d26529d9ccc2432c395aa
torsteoe <torsteoe@stud.ntnu.no>
2019-10-06 17:39:21 +0200

    implement authorization for site_admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
     def login_helper
-        if current_user.is_a?(User)
-            link_to "Logout", destroy_user_session_path, method: :delete
+        if current_user.is_a?(GuestUser)
+            (link_to "Signup", new_user_registration_path)+"<br>".html_safe+(link_to "Login", new_user_session_path)
         else
-            return (link_to "Signup", new_user_registration_path)+"<br>".html_safe+(link_to "Login", new_user_session_path)
+            link_to "Logout", destroy_user_session_path, method: :delete
         end
     end
 
