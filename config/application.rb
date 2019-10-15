4c4860a8fccc2967312646a7e31a3d83741674d9
Torstein Eliassen <torstein.eliassen@gmail.com>
2019-10-15 11:52:43 +0200

    twitter now working, not used in app

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,9 @@ Bundler.require(*Rails.groups)
 module DevcampPortfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.eager_load_paths << "#{Rails.root}/lib"
     config.load_defaults 5.1
     
-    config.eager_load_paths << "#{Rails.root}/root"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
