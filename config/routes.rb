f828a8999114508c6fd0790649f73fee2b5bc17f
Torstein Eliassen <torstein.eliassen@gmail.com>
2019-10-17 09:57:37 +0200

    create comment broadcast, configure web socket route, update comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
 	devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
 	resources :portfolios, except: [:show] do
         put :sort, on: :collection
@@ -16,5 +15,5 @@ Rails.application.routes.draw do
 			get :toggle_status
 		end
 	end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    mount ActionCable.server => '/cable'
 end
