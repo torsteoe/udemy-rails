7573e06c511801e7c64c3ba4883e3951b2032c71
torsteoe <torsteoe@stud.ntnu.no>
2019-10-27 11:27:53 +0100

    ran db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20191016112601) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
