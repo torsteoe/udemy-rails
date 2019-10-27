404ca78f0c74ee9c3b64d72b10e59f75141af0d6
torsteoe <torsteoe@stud.ntnu.no>
2019-10-27 09:59:48 +0100

    update secret key base call

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module DevcampPortfolio
     # Initialize configuration defaults for originally generated Rails version.
     config.eager_load_paths << "#{Rails.root}/lib"
     config.load_defaults 5.1
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
